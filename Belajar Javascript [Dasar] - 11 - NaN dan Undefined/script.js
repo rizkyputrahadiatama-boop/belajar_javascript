769f25b18a158af124bb0d047f5a77b92da12ef2
rizkyputrahadiatama-boop <rizkyputrahadiatama@gmail.com>
2026-07-23 16:33:41 +0700

    Belajar Javascript [Dasar] - 11 - NaN dan Undefined

---
+++ Belajar Javascript [Dasar] - 11 - NaN dan Undefined/script.js
@@ -0,0 +1,17 @@
+//NaN dan undefined
+
+//NaN = Not a Number
+console.log(`akar dari -1 :${Math.sqrt(-1)}`);
+console.log(`"usep" / "ramlan" :${"ucup" / "ramlan"}`);
+console.log(`"usep" * "ramlan" :${"ucup" * "ramlan"}`);
+console.log(`"usep" + "ramlan" :${"ucup" + "ramlan"}`);
+console.log(`"usep" - "ramlan" :${"ucup" - "ramlan"}`);
+
+let data = parseInt("test123");
+console.log(data + 5); //NaN ini tidak bisa dioprasikan lagi
+
+//undefined - nilai yang belum tahu
+let a;
+console.log(`a = ${a}`);
+console.log(Math.sqrt(4));
+console.log(console.log("test"));
