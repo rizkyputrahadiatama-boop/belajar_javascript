8c3bff68cd485723721322e430a3c532032eb744
rizkyputrahadiatama-boop <rizkyputrahadiatama@gmail.com>
2026-07-23 11:58:10 +0700

    Belajar Javascript [Dasar] - 05 - Tipe Data

---
+++ Belajar Javascript [Dasar] - 05 - Tipe Data/script.js
@@ -0,0 +1,24 @@
+//ini adalah tutor tentang data
+
+let dataString = "ramlan" //string
+console.log(dataString);
+console.log(typeof dataString)
+
+let dataNumber = 16.324; // number
+console.log(dataNumber);
+console.log(typeof dataNumber);
+
+let dataBoolean = false; //boolean
+console.log(dataBoolean);
+console.log(typeof dataBoolean);
+
+dataString = 2;
+console.log(dataString);
+console.log(typeof dataString);
+
+let dataKosong; //undefined
+console.log(dataKosong);
+console.log(typeof dataKosong);
+dataKosong = "usep";
+console.log(dataKosong);
+console.log(typeof dataKosong);
