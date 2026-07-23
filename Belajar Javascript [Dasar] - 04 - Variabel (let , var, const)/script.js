4a2ed52d428299530f347e6de9e3bd385cd9e610
rizkyputrahadiatama-boop <rizkyputrahadiatama@gmail.com>
2026-07-23 11:33:17 +0700

    Belajar Javascript [Dasar] - 04 - Variabel (let , var, const)

---
+++ Belajar Javascript [Dasar] - 04 - Variabel (let , var, const)/script.js
@@ -0,0 +1,45 @@
+// jadi ini adalah komen
+
+//variable dengan isi
+let nama = "Ramlan Spakbor";
+
+//tampilkan data
+console.log(nama);
+
+//mengubah nilai nama
+nama = "Usep MBG";
+console.log(nama);
+
+//variable dengan var
+var namaDepan = "Ramlan";
+console.log(namaDepan);
+namaDepan = "Usep";
+console.log(namaDepan);
+
+//kelakuaan  dari let 
+let namaBelakang = "Spakbor"
+{
+    let namaBelakang = "MBG";
+     console.log(namaBelakang);
+}
+console.log(namaBelakang);
+
+//kelakuaan dari var
+var namaTengah = "Keren";
+{
+    var namaTengah = "Ganteng"
+    console.log(namaTengah)
+}
+console.log(namaTengah);
+
+//kasus khusus tanpa keyword hanya jadi var
+gorengan = "Bala-Bala";
+{
+    gorengan = "Basreng";
+}
+console.log(gorengan);
+
+//const
+const TTL = " 19 Agustus 2009"
+console.log(TTL);
+//TTL tidak bisa diubah karena sudah konstan
