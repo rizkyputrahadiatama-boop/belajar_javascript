2b235defaef299e328608f6902b94f4e62a69b34
rizkyputrahadiatama-boop <rizkyputrahadiatama@gmail.com>
2026-07-24 15:04:22 +0700

    Belajar Javascript [Dasar] - 17 - IF Statement (Percabangan)

---
+++ Belajar Javascript [Dasar] - 17 - IF Statement (Percabangan)/script.js
@@ -0,0 +1,11 @@
+;let nama = "usep";
+
+console.log("nama" + "=" + nama);
+
+if(nama==="usep"){
+    console.log("Haii Usepp");
+}else{
+  console.log("au ah bukan usep")
+}
+
+console.log("akhir dari program");
