88a5328d85cfe66a6204e8c696484a6e266e9203
rizkyputrahadiatama-boop <rizkyputrahadiatama@gmail.com>
2026-07-24 15:20:41 +0700

    Belajar Javascript [Dasar] - 18 - Else IF & Switch Case

---
+++ Belajar Javascript [Dasar] - 18 - Else IF dan Switch Case/script.js
@@ -0,0 +1,22 @@
+console.log("mulai");
+
+let nama = "usep";
+
+if (nama === "usep"){
+  console.log("hai ramlan");
+}else if(nama === "ramlan"){
+  console.log("hai ramlan keren");
+}else{
+  console.log("kamu buka ramlan");
+}
+
+switch(nama){
+  case "usep":
+    console.log("switch case usep");
+    break;
+  case "ramlan":
+    console.log("switch case ramlan");
+    break;
+  default:
+    console.log("switch case nobody");
+}
