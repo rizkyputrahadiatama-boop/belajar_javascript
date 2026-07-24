ab9deced76bc6c8a84233fb2513e2eec55474e86
rizkyputrahadiatama-boop <rizkyputrahadiatama@gmail.com>
2026-07-24 10:28:19 +0700

    Belajar Javascript [Dasar] - 13 - Operasi Aritmatika

---
+++ Belajar Javascript [Dasar] - 13 - Operasi Aritmatika/script.js
@@ -0,0 +1,39 @@
+//operasi aritmatika
+
+//eksperesi dan statement
+
+// */+- 
+//* <- pangkat
+// ++ <- increment
+// -- <- decrement
+//% <- module
+
+let x = 10;
+console.log(x + 1);
+console.log(x - 1);
+console.log(x / 4);
+console.log(x * 2);
+
+//aritmatika khusus di javascrpt
+console.log(x % 3); //sisa pembagian
+console.log(x ** 2);
+
+//pemrograman biasa
+x = x + 1;
+console.log(x);
+x = x + 1;
+console.log(x);
+x++;
+x++;
+console.log(x);
+
+x = x - 1;
+console.log(x);
+x--;
+console.log(x);
+
+//studi kasus
+let y = 1 + "1";
+console.log(y);
+y = 1 + parseInt("1");
+console.log(y);
