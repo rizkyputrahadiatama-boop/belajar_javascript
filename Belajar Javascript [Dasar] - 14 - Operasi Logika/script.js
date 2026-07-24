81c24c6fe3530e47a9c438ba14892db87fb0ce0a
rizkyputrahadiatama-boop <rizkyputrahadiatama@gmail.com>
2026-07-24 10:59:06 +0700

    Belajar Javascript [Dasar] - 14 - Operasi Logika

---
+++ Belajar Javascript [Dasar] - 14 - Operasi Logika/script.js
@@ -0,0 +1,55 @@
+//operator logika
+
+//And, OR, Not
+
+let data_bool = true;
+console.log(data_bool);
+
+//Not
+console.log(!data_bool);//ngeflip data boolean
+console.log(!!data_bool);
+console.log(!1);
+console.log(!0);
+
+let is_keren = true;
+let is_jelek = !is_keren;
+
+//OR (operassi dua variable boolean)
+//A     true true false false
+//B     truw false true false
+//hasil true true true false
+
+//A     1 1 0 0
+//B     1 0 1 0
+//hasil 1 1 1 0
+
+console.log(`true || true, Hasil = ${true || true}`);
+console.log(`true || false, Hasil = ${true || false}`);
+console.log(`false || true, Hasil = ${false || true}`);
+console.log(`false || false, Hasil = ${false || false}`);
+
+let makan = false;
+let minum = false;
+
+let sudah_menyantap = makan || minum;
+console.log(`sudah menyantap = ${sudah_menyantap}`);
+
+//AND -> && (operasi antara dua variable boolean)
+//A     true true false false
+//B     truw false true false
+//hasil true false false false
+
+//A     1 1 0 0
+//B     1 0 1 0
+//hasil 1 0 0 0 
+
+console.log(`true && true, Hasil = ${true || true}`);
+console.log(`true && false, Hasil = ${true || false}`);
+console.log(`false && true, Hasil = ${false || true}`);
+console.log(`false && false, Hasil = ${false || false}`);
+
+makan = true;
+minum = true;
+
+let sudah_makmin = makan && minum;
+console.log(`sudah makan dan minum? = ${sudah_makmin}`);
