dce2928094828f6cfcbd25f78f86166a423fb308
rizkyputrahadiatama-boop <rizkyputrahadiatama@gmail.com>
2026-07-25 13:18:31 +0700

    Belajar Javascript [Dasar] - 30 - Argumen Fungsi

---
+++ Belajar Javascript [Dasar] - 30 - Argumen Fungsi/script.js
@@ -0,0 +1,37 @@
+//fungsi
+
+//function (argument){
+  //body program
+
+  //return value;
+//}
+
+//deklarasi fungsi
+function sayHello(){
+  let data = "hallo apa kabar";
+  console.log(data);
+}
+
+function sayKeren(){
+  let data = "hai keren";
+  console.log(data)
+}
+
+function say(dataInput){
+  console.log(dataInput);
+}
+
+//main program
+say("halo apa bakar");
+say("hai bakar");
+say("usep dimana oy");
+
+function tambah(angka1,angka2){
+  let hasil = angka1 + angka2;
+  let message = `${angka1} + ${angka2} = ${hasil}`;
+  console.log(hasil);
+}
+
+tambah(10,6);
+tambah(299,98);
+tambah(10);
