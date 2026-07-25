39fd76e3bf54726bb4a332a22efa6f07095c3197
rizkyputrahadiatama-boop <rizkyputrahadiatama@gmail.com>
2026-07-25 11:56:42 +0700

    Belajar Javascript [Dasar] - 29 - Pengenalan Fungsi

---
+++ Belajar Javascript [Dasar] - 29 - Pengenalan Fungsi/script.js
@@ -0,0 +1,23 @@
+//fungsi
+
+//function (argument){
+  //body program
+
+  //return value;
+//}
+
+//deklarasi fungsi
+function sayHello(){
+  let data = "hallo apa kabar";
+  console.log(data);
+}
+
+function sayKeren(){
+  let data = "hai keren";
+  console.log(data)
+}
+
+//main program
+sayHello();
+sayKeren();
+sayHello();
