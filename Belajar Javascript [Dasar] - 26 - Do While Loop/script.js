a0505028e73139530e1bf46f4cf93847427f1fbd
rizkyputrahadiatama-boop <rizkyputrahadiatama@gmail.com>
2026-07-25 10:34:49 +0700

    Belajar Javascript [Dasar] - 26 - Do While Loop

---
+++ Belajar Javascript [Dasar] - 26 - Do While Loop/script.js
@@ -0,0 +1,17 @@
+//do while
+
+console.log("awal program");
+
+let uang = 0;
+
+while(uang > 0){
+  console.log("kita bisa jajan ciki pakai while loop");
+}
+
+let saldo = 0;
+
+do {
+  console.log("kita bisa jajan ciki pakai do while loop");
+} while (saldo > 0);
+
+console.log("akhir program");
